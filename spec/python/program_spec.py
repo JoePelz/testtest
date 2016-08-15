23768d79fb26b5ee7f9b521a5b0ba603ac87f4ec
JoePelz <joe.pelz@gmail.com>
2016-08-15 15:55:27 -0700

    moved python test

--- spec/python/program_spec.py
+++ spec/python/program_spec.py
@@ -1,16 +1,29 @@
-import program as p
+import os
 import sys
 
+path = os.path.dirname(os.path.realpath(__file__))
+print("path is " + path)
+path = path[:path.rfind("/")]
+path = path[:path.rfind("/")]
+print("adding path: " + path);
+sys.path.append(path)
+
+
+for i in sys.path:
+    print i
+
+import program as p
+
 if (p.add(99, 1) != 100):
-  sys.exit(1)
+    sys.exit(1)
 
 if (p.sub(99, 50) != 49):
-  sys.exit(2)
+    sys.exit(2)
 
 if (p.mult(12, 10) != 120):
-  sys.exit(3)
+    sys.exit(3)
 
 if (p.div(12, 4) != 3):
-  sys.exit(4)
+    sys.exit(4)
 
 sys.exit(0)
